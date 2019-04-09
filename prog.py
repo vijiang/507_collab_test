882bfd81b50438bc6698207802544eb1421b5f1a
Stephanie Choi <choisteph@0587339954.wireless.umich.net>
2019-04-09 11:16:39 -0400

    more comments

--- prog.py
+++ prog.py
@@ -1,8 +1,9 @@
-import random
+import random #idk why
+import json #idk why
 
-def num_vowels(s):
-    vowels=0
-    for letter in s:
-        if letter in [a,e,i,o,u]:
-            vowels+=1
-    print(vowels)
+def num_vowels(s): #return vowels
+    vowels=0 #initialize
+    for letter in s: #iterate
+        if letter in [a,e,i,o,u]: #if letter is vowel (list of vowels)
+            vowels+=1 #add one
+    print(vowels) #print num_vowels
